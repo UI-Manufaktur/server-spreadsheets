450c6fbe09e96a50b0f062f094d5127d1d35e3dc
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-22 20:56:44 +0200

    Update package.d

--- source/servers/spreadsheets/controllers/package.d
+++ source/servers/spreadsheets/controllers/package.d
@@ -1,1 +1,5 @@
+module servers.spreadsheets.controllers;
 
+public import servers.spreadsheets.controllers.actions;
+public import servers.spreadsheets.controllers.apis;
+public import servers.spreadsheets.controllers.pages;
